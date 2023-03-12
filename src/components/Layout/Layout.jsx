bbbd2e3dce7679915b4b33a7f72d2887eee69f30
IrynaKniazieva <gleb0812@gmail.com>
2023-03-13 00:30:41 +0200

    fixed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import { Container } from './Layout.stuled';
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 
-import NavAuth from "../NavAuth/NavAuth"
+// import NavAuth from "../NavAuth/NavAuth"
 import AppBar from 'components/AppBar/AppBar';
 
 export const Layout = () => {
