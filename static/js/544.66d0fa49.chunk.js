92f977af9f7aa64c7b11a8ff2892425b25f32e45
IrynaKniazieva <103584399+IrynaKniazieva@users.noreply.github.com>
2023-03-14 23:30:12 +0000

    Deploying to gh-pages from @ IrynaKniazieva/goit-react-hw-08-phonebook@6fd2faa1286364adc0ee913b3e9d5271d109126b 🚀

---
+++ static/js/544.66d0fa49.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{9544:function(e,t,n){n.r(t);var r=n(4554),a=n(890),c=n(184);t.default=function(){return(0,c.jsx)("main",{style:{textAlign:"center"},children:(0,c.jsx)(r.Z,{sx:{paddingTop:"24px"},children:(0,c.jsx)(a.Z,{variant:"h4",gutterBottom:!0,children:"REGISTER AND CREATE YOUR CONTACT BOOK"})})})}}}]);
+//# sourceMappingURL=544.66d0fa49.chunk.js.map
